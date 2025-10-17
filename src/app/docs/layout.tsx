78beaa3766940c31725f8ac1c3083852d4f120e1
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-10-17 10:44:43 +0800

    feat(docs): refactor layout to use docsOptions for improved structure and GitHub info

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,10 +1,25 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { baseOptions } from '@/lib/layout.shared';
 import { source } from '@/lib/source';
+import { GithubInfo } from 'fumadocs-ui/components/github-info';
+import { DocsLayout, type DocsLayoutProps } from 'fumadocs-ui/layouts/docs';
+
+const docsOptions: DocsLayoutProps = {
+  ...baseOptions(),
+  tree: source.pageTree,
+  links: [
+    {
+      type: 'custom',
+      children: (
+        <GithubInfo owner="ukeSJTU" repo="nomad" className="lg: -mx-2" />
+      ),
+    },
+  ],
+};
+
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
+    <DocsLayout {...docsOptions}>
       {children}
     </DocsLayout>
   );

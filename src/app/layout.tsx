d0335abcb3a441fd62981763d2b5bde8ce5a916b
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-10-17 18:48:06 +0800

    feat(layout): add construction banner to layout for site status indication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/app/global.css';
+import { Banner } from 'fumadocs-ui/components/banner';
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import 'katex/dist/katex.css';
+import { Construction } from "lucide-react";
 import { Inter } from 'next/font/google';
 
 const inter = Inter({
@@ -11,6 +13,7 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+        <Banner id="global-banner" variant="rainbow"><Construction />This site is under construction.</Banner>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
